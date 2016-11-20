c6c5469ca6b2d5a58f719167171f8f8c396da2ae
silver.sun <itisagift@yeah.net>
2016-11-20 23:15:40 +0800

    finding ng2

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,35 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+"use strict";
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+scotchApp.config('$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            //templateUrl : '../pages/home.html',
+            template : 'shit'
+            //controller : 'mainController'
+        })
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller : 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller : 'contactController'
+        });
+});
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+scotchApp.controller('mainController', function ($scope) {
+    $scope.message = "Everyonex come and see how good i look";
+});
 
-app.initialize();
+
+scotchApp.controller('aboutController', function ($scope) {
+    $scope.message = "about";
+});
+
+
+scotchApp.controller('contactController', function ($scope) {
+    $scope.message = "actafbout";
+});
